ddf51d8f9002dd3bcb7dfb7a112f391b12506c33
GolovanovAlex <Golovanov.a.a@yandex.ru>
2022-10-18 19:05:05 +0300

    test 1

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,8 @@
+// ---- ---- menu btn on click ---- ---- //
+const btnMenu = document.querySelector('.menu-btn');
+
+btnMenu.addEventListener('click', () => {
+  btnMenu.classList.toggle('menu-btn--active');
+});
+
 $(function () {});
